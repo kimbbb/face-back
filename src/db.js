b96ce21eafa54fbcd480dfa278d87f3235665b03
김민지 <wehouse201@naver.com>
2022-11-08 14:09:49 +0900

    board

--- src/db.js
+++ src/db.js
@@ -8,8 +8,8 @@ const connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  dateStrings: "date",
 });
 
-console.log(connection)
 
 export default connection;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import authRouter from "./router/authRouter";
-import cors from "cors";
 import bodyParser from "body-parser";
+import boardRouter from "./router/BoardRouter";
 
 const app = express();
 const PORT = 8081;
@@ -18,6 +18,7 @@ app.use(allowCrossDomain)
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use("/auth", authRouter);
+app.use("/board", boardRouter);
 
 // 8080 포트번호로 서버를 킨다
 app.listen(PORT, () => {
